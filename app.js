33309e9b3b5d4ac88a2c24e1a8a703489f9ad58c
ortub10 <ortub10@gmail.com>
2022-06-17 16:19:42 +0300

    add other put

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import {
   addAccount,
   depositing,
   updateCredit,
+  cashWithdrawal,
+  transferring,
 } from "./users.js";
 
 const app = express();
@@ -24,7 +26,7 @@ app.get("/users/:id", (req, res) => {
 });
 
 app.post("/users", (req, res) => {
-  let { userId, cash, credit } = req.body;
+  let { userId, cash, credit } = req.query;
   if (cash === undefined) cash = 0;
   if (credit === undefined) credit = 0;
   const add = addUser({ userId, cash, credit });
@@ -45,11 +47,12 @@ app.put("/users/add-account/:id", (req, res) => {
   }
 });
 
-app.put("/users/cash/:id", (req, res) => {
-  const id = req.params.id;
-  const { numberToAdd, accountNunmer } = req.body;
+app.put("/users/cash/:id/:accountNunmer", (req, res) => {
+  const { id, accountNunmer } = req.params;
+  let { numberToAdd } = req.query;
+  if (numberToAdd === null) numberToAdd = 0;
 
-  const deposit = depositing(id, numberToAdd, accountNunmer);
+  const deposit = depositing(id, +numberToAdd, accountNunmer);
   if (deposit === "deposited") {
     res.send(getUsers());
   } else {
@@ -57,17 +60,49 @@ app.put("/users/cash/:id", (req, res) => {
   }
 });
 
-app.put("/users/credit/:id", (req, res) => {
-  const id = req.params.id;
-  const { numberToAdd, accountNunmer } = req.body;
+app.put("/users/credit/:id/:accountNunmer", (req, res) => {
+  const { id, accountNunmer } = req.params;
+  const { numberToAdd } = req.query;
+  if (numberToAdd === null) numberToAdd = 0;
 
-  const update = updateCredit(id, numberToAdd, accountNunmer);
+  const update = updateCredit(id, +numberToAdd, accountNunmer);
   if (update === "updated") {
     res.send(getUsers());
   } else {
     res.send(update);
   }
 });
+
+app.put("/users/withdrawal/:id/:accountNunmer", (req, res) => {
+  const { id, accountNunmer } = req.params;
+  const { cashToGet } = req.query;
+  const withdrawal = cashWithdrawal(id, +cashToGet, accountNunmer);
+  if (withdrawal === "withdrawaled") {
+    res.send(getUsers());
+  } else {
+    res.send(withdrawal);
+  }
+});
+
+app.put(
+  "/users/transference/:idSend/:accountNunmerSend/:idGet/:accountNunmerGet",
+  (req, res) => {
+    const { idSend, accountNunmerSend, idGet, accountNunmerGet } = req.params;
+    const { cashTotransfer } = req.query;
+    const transfer = transferring(
+      idSend,
+      accountNunmerSend,
+      idGet,
+      accountNunmerGet,
+      +cashTotransfer
+    );
+    if (transfer === "transfered") {
+      res.send(getUsers());
+    } else {
+      res.send(transfer);
+    }
+  }
+);
 
 app.listen(5000, () => {
   console.log(`listen to pore ${PORT}`);
--- users.js
+++ users.js
@@ -19,7 +19,7 @@ export const getUsers = () => {
 export const getUser = (id) => {
   const user = findUser(id);
   if (user !== undefined) return user;
-  return "user id not exist";
+  return "User id not exist";
 };
 
 export const addUser = (newUser) => {
@@ -56,7 +56,7 @@ export const addAccount = (id) => {
     fs.writeFileSync("users.json", JSON.stringify(users));
     return "adding";
   }
-  return "user id not exist";
+  return "User id not exist";
 };
 
 export const depositing = (id, number, accountNunmer) => {
@@ -78,15 +78,15 @@ export const depositing = (id, number, accountNunmer) => {
       fs.writeFileSync("users.json", JSON.stringify(users));
       return "deposited";
     } else {
-      return "accounrt number not exist";
+      return "Accounrt number not exist";
     }
   }
 
-  return "user id not exist";
+  return "User id not exist";
 };
 
 export const updateCredit = (id, number, accountNunmer) => {
-  if (number < 0) return "your uddted is negative number";
+  if (number < 0) return "your updted is negative number";
 
   const users = getUsers();
   const user = findUser(id);
@@ -104,9 +104,101 @@ export const updateCredit = (id, number, accountNunmer) => {
       fs.writeFileSync("users.json", JSON.stringify(users));
       return "updated";
     } else {
-      return "accounrt number not exist";
+      return "Accounrt number not exist";
     }
   }
 
-  return "user id not exist";
+  return "User id not exist";
+};
+
+export const cashWithdrawal = (id, cashToGet, accountNunmer) => {
+  if (cashToGet < 0) return "your cash withdrawal is negative number";
+
+  const users = getUsers();
+  const user = findUser(id);
+  const userIndex = users.findIndex((user) => user.userId == id);
+
+  if (user !== undefined) {
+    const accountIndex = user.acocunts.findIndex(
+      (account) => account.number_account === accountNunmer
+    );
+    const account = user.acocunts[accountIndex];
+    if (account !== undefined) {
+      if (cashToGet < account.cash + account.credit) {
+        const udtadeAccount = { ...account, cash: account.cash - cashToGet };
+        user.acocunts[accountIndex] = udtadeAccount;
+        users[userIndex] = user;
+        fs.writeFileSync("users.json", JSON.stringify(users));
+        return "withdrawaled";
+      } else {
+        return "The balance does not allow withdrawal";
+      }
+    } else {
+      return "Accounrt number not exist";
+    }
+  }
+
+  return "User id not exist";
+};
+
+export const transferring = (
+  idSend,
+  accountNunmerSend,
+  idGet,
+  accountNunmerGet,
+  cashTotransfer
+) => {
+  if (cashTotransfer < 0) return "your transference is negative number";
+
+  const users = getUsers();
+  const userSendIndex = users.findIndex((user) => user.userId == idSend);
+  const userGetIndex = users.findIndex((user) => user.userId == idGet);
+  const userSend = users[userSendIndex];
+  const userGet = users[userGetIndex];
+
+  if (userSend !== undefined && userGet !== undefined) {
+    const accountIndexSend = userSend.acocunts.findIndex(
+      (account) => account.number_account === accountNunmerSend
+    );
+
+    const accountIndexGet = userGet.acocunts.findIndex(
+      (account) => account.number_account === accountNunmerGet
+    );
+
+    const accountSend = userSend.acocunts[accountIndexSend];
+    const accountGet = userGet.acocunts[accountIndexGet];
+
+    if (accountSend !== undefined && accountGet !== undefined) {
+      if (accountGet === accountSend) {
+        return "Same account";
+      }
+
+      if (cashTotransfer < accountSend.cash + accountSend.credit) {
+        const udtadeAccountSend = {
+          ...accountSend,
+          cash: accountSend.cash - cashTotransfer,
+        };
+        userSend.acocunts[accountIndexSend] = udtadeAccountSend;
+        users[userSendIndex] = userSend;
+
+        console.log();
+        const udtadeAccountGet = {
+          ...accountGet,
+          cash: accountGet.cash + cashTotransfer,
+        };
+        userGet.acocunts[accountIndexGet] = udtadeAccountGet;
+        users[userGetIndex] = userGet;
+
+        fs.writeFileSync("users.json", JSON.stringify(users));
+
+        return "transfered";
+      } else {
+        return "The balance does not allow transference";
+      }
+    } else {
+      return "one of accounrt numbers not exist";
+    }
+  }
+
+  return "one of user ids not exist";
 };
