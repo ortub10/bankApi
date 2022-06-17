fbed85af3bb897863941c2c8f2c1f2cc4ea3f3a7
ortub10 <ortub10@gmail.com>
2022-06-17 13:50:54 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+import express from "express";
+import { getUser, getUsers, addUser, addAccount, depositing } from "./users.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/users", (req, res) => {
+  res.send(getUsers());
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(getUser(id));
+});
+
+app.post("/users", (req, res) => {
+  let { userId, cash, credit } = req.body;
+  if (cash === undefined) cash = 0;
+  if (credit === undefined) credit = 0;
+  const add = addUser({ userId, cash, credit });
+  if (add === "adding") {
+    res.send(getUsers());
+  } else {
+    res.send(add);
+  }
+});
+
+app.put("/users/add-account/:id", (req, res) => {
+  const id = req.params.id;
+  const add = addAccount(id);
+  if (add === "adding") {
+    res.send(getUsers());
+  } else {
+    res.send(add);
+  }
+});
+
+app.put("/users/cash/:id", (req, res) => {
+  const id = req.params.id;
+  const { numberToAdd, accountNunmer } = req.body;
+
+  const deposit = depositing(id, numberToAdd, accountNunmer);
+  if (deposit === "deposited") {
+    res.send(getUsers());
+  } else {
+    res.send(deposit);
+  }
+});
+
+app.listen(5000, () => {
+  console.log(`listen to pore ${PORT}`);
+});
---
+++ users.js
@@ -0,0 +1,86 @@
+import fs from "fs";
+
+const findUser = (id) => {
+  const users = getUsers();
+  const user = users.find((userElement) => userElement.userId == id);
+  return user;
+};
+
+export const getUsers = () => {
+  try {
+    const dataBuffer = fs.readFileSync("users.json");
+    const data = JSON.parse(dataBuffer);
+    return data;
+  } catch (e) {
+    return [];
+  }
+};
+
+export const getUser = (id) => {
+  const user = findUser(id);
+  if (user !== undefined) return user;
+  return "user id not exist";
+};
+
+export const addUser = (newUser) => {
+  const users = getUsers();
+  if (newUser.userId === undefined) return "User id missing";
+  const user = findUser(newUser.userId);
+  if (user === undefined) {
+    const newUserObj = {
+      userId: newUser.userId,
+      acocunts: {
+        number_account: `${newUser.userId}-1`,
+        cash: newUser.cash,
+        credit: newUser.credit,
+      },
+    };
+    users.push(newUserObj);
+    fs.writeFileSync("users.json", JSON.stringify(users));
+    return "adding";
+  }
+  return "User id already exist ";
+};
+
+export const addAccount = (id) => {
+  const users = getUsers();
+  const userIndex = users.findIndex((userElement) => userElement.userId == id);
+  const user = users[userIndex];
+  if (user !== undefined) {
+    const newAcount = {
+      number_account: `${id}-${user.acocunts.length + 1}`,
+      cash: 0,
+      credit: 0,
+    };
+    users[userIndex] = { ...user, acocunts: [...user.acocunts, newAcount] };
+    fs.writeFileSync("users.json", JSON.stringify(users));
+    return "adding";
+  }
+  return "user id not exist";
+};
+
+export const depositing = (id, number, accountNunmer) => {
+  if (number < 0) return "your deposited is negative number";
+
+  const users = getUsers();
+  const user = findUser(id);
+  const userIndex = users.findIndex((user) => user.userId == id);
+
+  if (user !== undefined) {
+    const accountIndex = user.acocunts.findIndex(
+      (account) => account.number_account === accountNunmer
+    );
+    const account = user.acocunts[accountIndex];
+    if (account !== undefined) {
+      const udtadeAccount = { ...account, cash: account.cash + number };
+      user.acocunts[accountIndex] = udtadeAccount;
+      users[userIndex] = user;
+      fs.writeFileSync("users.json", JSON.stringify(users));
+      return "deposited";
+    } else {
+      return "accounrt number not exist";
+    }
+  }
+
+  return "user id not exist";
+};
