c4170507ab024f7ca81916e22a5a5a732c0f892a
ortub10 <ortub10@gmail.com>
2022-06-17 17:14:03 +0300

    instal dotenv

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import dotenv from "dotenv";
 import {
   getUser,
   getUsers,
@@ -12,6 +13,7 @@ import {
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded());
